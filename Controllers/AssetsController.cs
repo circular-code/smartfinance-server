f54de029ce778b5b75953b37f67e4feee2e85324
Stephanos <stephanos@malikotsinas.de>
2021-03-09 23:29:44 +0100

    added post and delete endpoints for assets

--- Controllers/AssetsController.cs
+++ Controllers/AssetsController.cs
@@ -3,13 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 using Smartfinance_server.Data;
 using Smartfinance_server.Models;
 
+// https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/routing
+// https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio
+// https://docs.microsoft.com/en-us/dotnet/api/system.web.http.apicontroller?view=aspnetcore-2.2
+
 namespace Smartfinance_server.Controllers
+
 {
     [Route("api/assets")]
     [ApiController]
     public class AssetsController : ControllerBase
     {
-
         private readonly QueryEngine _qe;
 
         public AssetsController(QueryEngine qe)
@@ -21,64 +25,75 @@ namespace Smartfinance_server.Controllers
         //get all assets
         //TODO: limit with skip & take, filter etc. like devextreme params
         [HttpGet]
-        public ActionResult <IEnumerable<Asset>> GetAllAssets() {
-            var assets = _qe.GetAllAssets();
-
-            return Ok(assets);
+        public ActionResult<IEnumerable<Asset>> GetAllAssets() {
+            return Ok(_qe.GetAllAssets());
         }
 
         //GET api/assets/id
         //get a specific assets
         [HttpGet("{id}")]
-        public ActionResult <Asset> GetAssetById(uint id)
+        public ActionResult<Asset> GetAsset(uint id)
         {
-            var asset = _qe.GetAssetById(id);
-            return Ok(asset);
-        }
+            var asset = _qe.GetAsset(id);
 
-        //PUT api/assets/id
-        //update/replace the whole asset
-        [HttpPut("{id}")]
-        public ActionResult <Asset> GetAssetById(uint id)
-        {
-            var asset = _qe.GetAssetById(id);
+            if (asset == null)
+                return NotFound();
+
             return Ok(asset);
         }
 
         //POST api/assets
         //create a asset in full
         [HttpPost]
-        public ActionResult <Asset> Create(Asset asset)
+        public ActionResult<Asset> CreateAsset(Asset asset)
         {
-            var asset = _qe.Create(asset);
-            return CreatedAtAction(asset);
+            var newAsset = _qe.CreateAsset(asset);
+
+            if (newAsset == null)
+                return NotFound();
+                
+            return NoContent();
         }
 
+        // since PATCH requres additional dependencies (jsonpatch), we are not going to use it
         //PATCH api/assets/id
         //update part of an asset
-        [HttpPatch("{id}")]
-        public ActionResult <Asset> Create(Asset asset)
-        {
-            var asset = _qe.CreateAsset(asset);
-            return CreatedAtAction(asset);
-        }
+        // [HttpPatch("{id}")]
+        // public ActionResult<Asset> UpdateAsset(uint id)
+        // {
+        //     return Ok(_qe.UpdateAsset(asset));
+        // }
 
-        //DELETE api/assets/id
-        //delete a specific asset
+        // we are missusing PUT to Update existing Assets instead of patch
+        //PUT api/assets/id
+        //replace asset if existing or create the asset if not existing
+        // [HttpPut("{id}")]
+        // public ActionResult <Asset> ReplaceAsset(uint id)
+        // {
+        //     return Ok(_qe.ReplaceAsset(id));
+        // }
+
+        // DELETE api/assets/id
+        // delete a specific asset
         [HttpDelete("{id}")]
-        public ActionResult <Asset> Create(Asset asset)
+        public ActionResult<Asset> DeleteAsset(uint id)
         {
-            var asset = _qe.CreateAsset(asset);
-            return CreatedAtAction(asset);
+            var asset = _qe.GetAsset(id);
+
+            if (asset == null)
+                return NotFound();
+
+            _qe.DeleteAsset(id);
+            return NoContent();
         }
 
         //HEAD api/assets
         //efficiently lookup whether large assets have been updated in conjunction with the ETag-header.
-        [HttpHead("{id}")]
-        public ActionResult <Asset> GetAssetById(uint id)
-        {
-            var asset = _qe.GetAssetById(id);
-            return Ok(asset);
-        }
+        // [HttpHead("{id}")]
+        // public ActionResult <Asset> GetAssetById(uint id)
+        // {
+        //     var asset = _qe.GetAssetById(id);
+        //     return Ok(asset);
+        // }
     }
 }
--- Data/QueryEngine.cs
+++ Data/QueryEngine.cs
@@ -14,8 +14,16 @@ namespace Smartfinance_server.Data
 
         // ExecuteNonQuery to insert, update, and delete data.
 
-        // ExecuteScalar to return a single value.
+        // ExecuteScalar to return a single value (first column of first row)
 
+        // Improve SQL Security with Stored procedures and parameters
+
+        // implement sql helper? https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlcommand?view=dotnet-plat-ext-5.0
+
+        // https://www.youtube.com/watch?v=fmvcAzHpsk8 Les Jackson MVC Rest API Course
+
+        #region Asset
+            
         public IEnumerable<Asset> GetAllAssets()
         {
             List<Asset> list = new List<Asset>();
@@ -23,9 +31,9 @@ namespace Smartfinance_server.Data
             using (MySqlConnection conn = DbContext.GetConnection())
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from Assets where id < 10", conn);
+                MySqlCommand cmd = new MySqlCommand("select * from Asset", conn);
 
-                using (var reader = cmd.ExecuteReader())
+                using (MySqlDataReader reader = cmd.ExecuteReader())
                 {
                     while (reader.Read())
                     {
@@ -35,12 +43,12 @@ namespace Smartfinance_server.Data
                             Id = Convert.ToInt32(reader["Id"]),
                             CreationDate = reader["CreationDate"].ToString(),
                             ContractDate = reader["ContractDate"].ToString(),
-                            CurrentValue = reader["CurrentValue"],
+                            CurrentValue = Convert.ToDecimal(reader["CurrentValue"]),
                             Currency = reader["Currency"].ToString(),
                             PrimaryTransactionId = Convert.ToInt32(reader["PrimaryTransactionId"]),
                             Description = reader["Description"].ToString(),
                             Type = reader["Type"].ToString(),
-                            CurrentQuantity = reader["CurrentQuantity"],
+                            CurrentQuantity = Convert.ToDecimal(reader["CurrentQuantity"]),
                             LiabilityIds = reader["LiabilityIds"].ToString(),
                             TransactionIds = reader["TransactionIds"].ToString()
                         });
@@ -50,7 +58,7 @@ namespace Smartfinance_server.Data
             return list;
         }
 
-        public Asset GetAssetById(uint id)
+        public Asset GetAsset(uint id)
         {
 
             Asset asset = new Asset();
@@ -58,7 +66,7 @@ namespace Smartfinance_server.Data
             using (MySqlConnection conn = DbContext.GetConnection())
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from Assets where id = " + id, conn);
+                MySqlCommand cmd = new MySqlCommand("select * from Asset where id = " + id, conn);
 
                 using (var reader = cmd.ExecuteReader())
                 {
@@ -69,38 +77,86 @@ namespace Smartfinance_server.Data
                             Id = Convert.ToInt32(reader["Id"]),
                             CreationDate = reader["CreationDate"].ToString(),
                             ContractDate = reader["ContractDate"].ToString(),
-                            CurrentValue = reader["CurrentValue"],
+                            CurrentValue = Convert.ToDecimal(reader["CurrentValue"]),
                             Currency = reader["Currency"].ToString(),
                             PrimaryTransactionId = Convert.ToInt32(reader["PrimaryTransactionId"]),
                             Description = reader["Description"].ToString(),
                             Type = reader["Type"].ToString(),
-                            CurrentQuantity = reader["CurrentQuantity"],
+                            CurrentQuantity = Convert.ToDecimal(reader["CurrentQuantity"]),
                             LiabilityIds = reader["LiabilityIds"].ToString(),
                             TransactionIds = reader["TransactionIds"].ToString()
-                        }
+                        };
                     }
                 }
             }
             return asset;
         }
 
-        public Asset Create(Asset asset)
+        public Asset CreateAsset(Asset asset)
         {
             using (MySqlConnection conn = DbContext.GetConnection())
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand("select * from Assets where id = " + id, conn);
 
-                cmd = new MySqlCommand("INSERT INTO mytable (Id,User,CreationDate,ContractDate,CurrentValue,Currency,PrimaryTransactionId,Description,Type,CurrentQuantity,LiabilityIds,TransactionIds) VALUES (@Id,@User,@CreationDate,@ContractDate,@CurrentValue,@Currency,@PrimaryTransactionId,@Description,@Type,@CurrentQuantity,@LiabilityIds,@TransactionIds)", conn);
-                //TODO find out how to use parameters
-                cmd.Parameters.Add("@Id", MySqlDbType.VarChar, 15, "Id" );
-                cmd.Parameters.Add("@name", MySqlDbType.VarChar, 15, "name" );
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO asset (User,CreationDate,ContractDate,CurrentValue,Currency,PrimaryTransactionId,Description,Type,CurrentQuantity,LiabilityIds,TransactionIds) VALUES (@User,@CreationDate,@ContractDate,@CurrentValue,@Currency,@PrimaryTransactionId,@Description,@Type,@CurrentQuantity,@LiabilityIds,@TransactionIds)", conn);
+                cmd.Parameters.Add("@User",                 MySqlDbType.VarChar).Value = asset.User;
+                cmd.Parameters.Add("@CreationDate",         MySqlDbType.VarChar).Value = asset.CreationDate;
+                cmd.Parameters.Add("@ContractDate",         MySqlDbType.VarChar).Value = asset.ContractDate;
+                cmd.Parameters.Add("@CurrentValue",         MySqlDbType.Decimal).Value = asset.CurrentValue;
+                cmd.Parameters.Add("@Currency",             MySqlDbType.VarChar).Value = asset.Currency;
+                cmd.Parameters.Add("@PrimaryTransactionId", MySqlDbType.UInt16 ).Value = asset.PrimaryTransactionId;
+                cmd.Parameters.Add("@Description",          MySqlDbType.VarChar).Value = asset.Description;
+                cmd.Parameters.Add("@Type",                 MySqlDbType.VarChar).Value = asset.Type;
+                cmd.Parameters.Add("@CurrentQuantity",      MySqlDbType.Decimal).Value = asset.CurrentQuantity;
+                cmd.Parameters.Add("@LiabilityIds",         MySqlDbType.VarChar).Value = asset.LiabilityIds;
+                cmd.Parameters.Add("@TransactionIds",       MySqlDbType.VarChar).Value = asset.TransactionIds;
 
                 cmd.ExecuteNonQuery();
                 conn.Close();
             }
             return asset;
         }
-        
+
+        //TODO:
+        // public Asset UpdateAsset(Asset asset)
+        // {
+        //     using (MySqlConnection conn = DbContext.GetConnection())
+        //     {
+        //         conn.Open();
+
+        //         MySqlCommand cmd = new MySqlCommand("INSERT INTO asset (User,CreationDate,ContractDate,CurrentValue,Currency,PrimaryTransactionId,Description,Type,CurrentQuantity,LiabilityIds,TransactionIds) VALUES (@User,@CreationDate,@ContractDate,@CurrentValue,@Currency,@PrimaryTransactionId,@Description,@Type,@CurrentQuantity,@LiabilityIds,@TransactionIds)", conn);
+        //         if (asset.User)
+        //         cmd.Parameters.Add("@User",                 MySqlDbType.VarChar).Value = asset.User;
+        //         cmd.Parameters.Add("@CreationDate",         MySqlDbType.VarChar).Value = asset.CreationDate;
+        //         cmd.Parameters.Add("@ContractDate",         MySqlDbType.VarChar).Value = asset.ContractDate;
+        //         cmd.Parameters.Add("@CurrentValue",         MySqlDbType.Decimal).Value = asset.CurrentValue;
+        //         cmd.Parameters.Add("@Currency",             MySqlDbType.VarChar).Value = asset.Currency;
+        //         cmd.Parameters.Add("@PrimaryTransactionId", MySqlDbType.UInt16 ).Value = asset.PrimaryTransactionId;
+        //         cmd.Parameters.Add("@Description",          MySqlDbType.VarChar).Value = asset.Description;
+        //         cmd.Parameters.Add("@Type",                 MySqlDbType.VarChar).Value = asset.Type;
+        //         cmd.Parameters.Add("@CurrentQuantity",      MySqlDbType.Decimal).Value = asset.CurrentQuantity;
+        //         cmd.Parameters.Add("@LiabilityIds",         MySqlDbType.VarChar).Value = asset.LiabilityIds;
+        //         cmd.Parameters.Add("@TransactionIds",       MySqlDbType.VarChar).Value = asset.TransactionIds;
+
+        //         cmd.ExecuteNonQuery();
+        //         conn.Close();
+        //     }
+        //     return asset;
+        // }
+
+        public void DeleteAsset(uint id)
+        {
+            using (MySqlConnection conn = DbContext.GetConnection())
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM asset WHERE id = " + id, conn);
+
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+
+        #endregion
     }
 }
